719c6a038bfea58ebfb95482a1eb0fe0616b5d72
PBK-B <pbk-b@PBK6.cn>
2024-09-13 20:38:25 +0800

    chore: config project

--- EFPHelper/main.m
+++ EFPHelper/main.m
@@ -6,12 +6,27 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "FileProvider/NSFileProviderManager.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
     }
-    printf("hello");
+    printf("hello\n");
+    
+    [NSFileProviderManager removeAllDomainsWithCompletionHandler:^(NSError * _Nullable error) {
+        NSFileProviderDomain *domain =
+                [[NSFileProviderDomain alloc] initWithIdentifier:@"cloud.lazycat.client"
+                                                     displayName:@"TestFPE"];
+        [NSFileProviderManager addDomain:domain completionHandler:^(NSError * _Nullable error) {
+            printf("err %s \n", [error.debugDescription UTF8String]);
+        }];
+        // url
+        
+        // XPC -> FP
+        
+    }];
+
     [[NSApplication sharedApplication] run];
     return 1;
 }
