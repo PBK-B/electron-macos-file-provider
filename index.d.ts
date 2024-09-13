2a4cc484751a0d7013d980e9bbdb9d799ee2c5a6
PBK-B <pbk-b@PBK6.cn>
2024-09-13 14:41:28 +0800

    fix: add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: Bin
+ * @Date: 2024-09-13
+ * @FilePath: /electron-macos-file-provider/index.d.ts
+ */
+
+declare module 'electron-macos-file-provider' {
+    function addDomain(identifier: string, displayName: string, callback: (...parameters: any) => void | undefined): void;
+    function removeAllDomains(): void;
+}
